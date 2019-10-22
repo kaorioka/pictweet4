ae1e0fd75b28b330bd7bfff7bf3d097d0463663c
Kaori Oka <kaorioka09jm@gmail.com>
2019-10-22 23:32:34 +0900

    add_create_action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,4 +7,14 @@ class TweetsController < ApplicationController
   def new
   end
 
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
+  end
+
+
 end
