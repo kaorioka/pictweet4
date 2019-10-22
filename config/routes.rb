55cb71193e30b30d08ea991b62af256916519409
Kaori Oka <kaorioka09jm@gmail.com>
2019-10-22 23:51:48 +0900

    update_tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'tweets' => 'tweets#index'
-  get   'tweets/new'  =>  'tweets#new'
-  post  'tweets'      =>  'tweets#create'
+  root  'tweets#index'                       #ルートパスの指定
+  get   'tweets'      =>  'tweets#index'     #ツイート一覧画面
+  get   'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
+  post  'tweets'      =>  'tweets#create' 
 end
